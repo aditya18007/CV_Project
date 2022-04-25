f40d1cbdbe65ea95b37c08525b2731c44bf3c171
Aditya Rathore <aditya18007@iiitd.ac.in>
2022-04-26 03:48:28 +0530

    Removed tensor creation from data reader.

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,12 +6,6 @@ import tqdm
 from CONST import DATASET_TYPES 
 from one_hot_encoding import one_hot_encode
 
-transform = transforms.Compose([
-    transforms.PILToTensor()
-])
-
-
-
 def read_data(dataset_type:str):
     if (dataset_type not in DATASET_TYPES):
         print(f"Invalid Dataset type requested ({DATASET_TYPES}). Type must be one of {DATASET_TYPES}")
@@ -26,9 +20,7 @@ def read_data(dataset_type:str):
         data_point = json.loads(json_str)
         image_path = f"Data/images/{data_point['image']}"
         img = Image.open(image_path)
-        img_tensor = transform(img)
-        
-        data_point['image_tensor'] = img_tensor
+
         data_point['image_PIL'] = img
         data_point['image_path'] = f"Data/images/{data_point['image']}"
         #Prevent some bad access from generic name
